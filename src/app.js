79c21aedc8fa94099e8ff39a20d12517c37be0c3
Craig Contreras <craig1contreras@gmail.com>
2022-02-23 13:58:25 -0500

    good luck lmao

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+let data = {};
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.post("/player_data", (req, res) => {
+    data = req.body;    
+    res.sendStatus(200);
+})
+
+app.get("/stats/:name", (req, res) => {
+    const {name} = req.params;
+    if (name in data) {
+        res.render("stats", data);
+    }
+})
+
+app.listen(80, () => {
+    console.log("listening on 80");
+})
